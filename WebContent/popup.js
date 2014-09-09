d1fabdf80f989eae896b9bc4603ef0942b0db10a
Andreas Sehr <andreas@mansehr.se>
2014-09-09 17:47:58 +0200

    Same client written with angularjs.

--- WebContent/popup.js
+++ WebContent/popup.js
@@ -1,70 +1,108 @@
-var SERVICE_URI = "http://pagediffer.appspot.com/mansehr_moviesearch";
+var SERVICE_URI = "http://mansehrmoviesearch.appspot.com/mansehr_moviesearch";
 
 var version = 2;
-var DEBUG = true;
+var DEBUG = false;
 var sleepIterations = 0;
-var supplierUrls = [];
-var supplierUse = {};
-var resultArray = [];
-var movieName = '';
 
-var data = new Object();
+angular.module('MovieSearchApp', []).
+controller('MovieSearchCtrl', function($scope) {
+	
+	if(DEBUG) {
+		$scope.mode = 'dev';
+	}
+	
+	/**
+	 * Initialize active supplier urls on pageload
+	 */
+	function initSupplierURLs() {
+		$scope.$apply(showInfo("Laddar filmtjänster"));
+		
+		var delta = 0;
+		$scope.lastAccess = localStorageLoad("mansehrSULastAccess");
+		if ($scope.lastAccess !== undefined) {
+			var laTime = new Date($scope.lastAccess);
+			delta = (new Date().getTime() - laTime);
+		}
+		
+		var urls = localStorageLoad("mansehrServiceUrls");
+		if (urls == undefined || isNaN(delta) || delta > 1800000) {
+			showInfo("Laddade filmtjänster från servern");
+			localStorageClear();
+			$.getJSON(SERVICE_URI, {
+					suppliers : version
+				})
+				.done(
+					function(data) {
+						parseUrls(data, true);
+						localStorageStore("mansehrServiceUrls", JSON
+								.stringify($scope.supplierUrls));
+						localStorageStore("mansehrSULastAccess",
+								new Date());
+						$scope.$apply("showForm = true");
+					})
+				.fail(
+					function(jqxhr, textStatus, error) {
+						var err = textStatus + ', ' + error;
+						console.log("Request Failed: " + err);
+						showError("Anropet för att hämta filmtjänster misslyckades. Kontrollera att du är ansluten till internet.");
+					});
+		} else {
+			showInfo("Laddade filmtjänster från cachen");
+			// Load cache
+			$scope.supplierUrls = JSON.parse(urls);
+			$scope.showForm = true;
+		}
+		resetMessage();
+	}
+	
+	function loadLastSearch() {
+		$scope.movieName = localStorageLoad("movieSearchQry");
+		if ($scope.movieName === undefined) {
+			$scope.movieName = "";
+		}
 
-// Handler for .ready() called.
-$(function() {
-
+		var res = localStorageLoad("movieSearchResult");
+		if (res !== undefined) {
+			$scope.result = JSON.parse(res);
+		} else {
+			resetResult();
+		}
+	}
+	
 	initSupplierURLs();
 
 	// Load last search
 	loadLastSearch();
 
-	$('#target').submit(function(form) {
 
-		if (supplierUrls.length == 0) {
-			$('#result').html('Inga filmtj&auml;nster laddade.');
+	$scope.formSubmit = function() {
+		if ($scope.supplierUrls.length == 0) {
+			$scope.errMsg = 'Inga filmtjänster laddade. Kontrollera internetanslutning. ';
 		}
 		try {
-			movieName = $('#movie_name').val();
-			if (movieName != '') {
-				$('#form_result').hide();
-				$('#result').text("");
-				$('#search_img').fadeIn();
-				localStorageStore("movieSearchQry", movieName);
+			if ($scope.movieName != '') {
+				$scope.showForm = false;
+				
+				localStorageStore("movieSearchQry", $scope.movieName);
 
 				resetResult();
 
 				searchSuppliers();
 
 				waitUntilSearchDone();
-
 			} else {
-				$('#result').html("Ange en film att s&ouml;ka efter");
+				showInfo("Ange en film att söka efter");
 			}
 		} catch (e) {
+			debug("Request Failed: " + e);
 			console.log("Request Failed: " + e);
+
+			$scope.showForm = true;
 		}
-		form.preventDefault();
-	});
-	
-	$('#settings_lnk').click(function () {
-		$('#suppliers_list').toggle('slow');
-	});
-	
-	resetSearch();
-});
+	};
 
-function loadLastSearch() {
-	var qry = localStorageLoad("movieSearchQry");
-	if (qry != undefined) {
-		$('#movie_name').val(qry);
-	}
 
-	var res = localStorageLoad("movieSearchResult");
-	if (res != undefined) {
-		resetResult();
-		parseJsonResult(res);
-	}
-}
+
 
 /**
  * Loop until all searchsuppliers are done
@@ -73,224 +111,113 @@ function loadLastSearch() {
  */
 function waitUntilSearchDone() {
 	// We search for max 10 sec, so the user dont have to wait to long
-	if (resultArray.length == 0 && ++sleepIterations < 20) {
+	if ($scope.result && $scope.result.good && $scope.result.good.length == 0 && ++sleepIterations < 20) {
 		setTimeout(waitUntilSearchDone, 500);
 		return;
 	}
-	resetSearch();
-}
-
-function resetSearch() {
 	sleepIterations = 0;
-
-	$('#search_img').hide();
-	$('#form_result').show();
-	$('#movie_name').focus();
+	$scope.$apply("showForm = true");
 }
 
 function resetResult() {
-	data.goodMatchResult = [];
-	data.otherResult = [];
+	$scope.result = {good: [], other: []};
 }
 
-/**
- * Parse the result from the parsing server
- */
-function parseResult(data) {
-	$('#result').text('');
-
-	data.goodMatchResult.sort(function(a, b) {
-		return a.price - b.price;
-	});
-	apendListToResult(data.goodMatchResult, 'Tr&auml;ffar:');
-
-	data.otherResult.sort(function(a, b) {
-		return a.movieName.toUpperCase().localeCompare(
-				b.movieName.toUpperCase());
-	});
-	apendListToResult(data.otherResult, 'Andra tr&auml;ffar:');
-	localStorageStore("movieSearchResult", JSON.stringify(data));
-}
-
-function apendListToResult(data, title) {
-	var items = [];
-
-	$.each(data, function(key, val) {
-		items.push(parseItem(key, val));
-	});
-	if (items.length > 0) {
-		$('<h2/>', {
-			'class' : 'list-title',
-			html : title
-		}).appendTo('#result');
-		
-		$('<ul/>', {
-			'class' : 'movie_list',
-			html : items.join('')
-		}).appendTo('#result');
-	}
-}
-
-function parseItem(key, val) {
-	var sup = supplierUrls[val.supplier];
-	return '<li id="' + key + '"><a href="' + val.url
-			+ '" target="_blank"><img src="' + val.imgUrl + '" alt="'
-			+ val.movieName + '" /></a>' + val.movieName + '<br/>Pris: '
-			+ val.price + ':-<br/><a href="' + val.url
-			+ '" target="_blank" class="logo" style="background: url('
-			+ sup.logoUrl + ') no-repeat;"></a></li>';
-}
-
-/**
- * Initialize active supplier urls on pageload
- */
-function initSupplierURLs() {
-	infoMsg("Laddar filmtjänster");
-	var urls = localStorageLoad("mansehrServiceUrls");
-	var lastAccess = localStorageLoad("mansehrSULastAccess");
-	supplierUse = localStorageLoad("mansehrSupplierUse");
-	// Skapa en associative array om den inte finns
-	if(supplierUse == undefined) {
-		supplierUse = {};
-	} else {
-		supplierUse = JSON.parse(supplierUse);
-	}
-	var delta = 0;
-	if (lastAccess != undefined) {
-		var laTime = new Date(lastAccess);
-		delta = (new Date().getTime() - laTime);
-	}
-	if (urls == undefined || isNaN(delta) || delta > 1800000) {
-		localStorageClear();
-		$
-				.getJSON(SERVICE_URI, {
-					suppliers : version
-				})
-				.done(
-						function(data) {
-							parseUrls(data, true);
-							localStorageStore("mansehrServiceUrls", JSON
-									.stringify(data));
-							localStorageStore("mansehrSULastAccess",
-									new Date());
-							localStorageStore("mansehrSupplierUse", 
-										JSON.stringify(supplierUse));
-							showForm();
-						})
-				.fail(
-						function(jqxhr, textStatus, error) {
-							var err = textStatus + ', ' + error;
-							console.log("Request Failed: " + err);
-							$('#errMsg')
-									.html(
-											"Anropet f&ouml;r att h&auml;mta filmtj&auml;nster misslyckades. Kontrollera att du &auml;r ansluten till internet.");
-						});
-	} else {
-		// Load cache
-		parseUrls(JSON.parse(urls), false);
-		showForm();
-	}
-	infoMsg("");
-}
 
 function parseUrls(data, forceUse) {
-	var settingsList = '';
-	$.each(data, function(key, val) {
-		var res = {};
-		res.id = val.supplierId;
-		res.url = val.url;
-		res.logoUrl = val.logoUrl;
-		res.name = val.name;
-		//supplierUrls.push(res);
-		supplierUrls[res.id] = res;
-		
-		if(forceUse) {
-			supplierUse[res.id] = true;
-		} else if(supplierUse[res.id] == undefined) {
-			supplierUse[res.id] = false;
-		}
-		settingsList += '<li><input type="checkbox" '+(supplierUse[res.id] ? 'checked' : '')+' id="'+res.id+'">'+res.id+'</input></li>';
+	$scope.supplierUrls = {};
+	angular.forEach(data, function(val, key) {
+		var res = {
+			id: val.supplierId,
+			url : val.url,
+			logoUrl : val.logoUrl,
+			name : val.name,
+			use: true,
+			hits: {good: "*", tot: "*"}
+		};
+		$scope.supplierUrls[res.id] = res;
 	});
-	
-	$('#suppliers_list').append(settingsList);
-	
-	$('#settings input').click(toggleUse);
 }
 
-function toggleUse(event) {
-	var id = event.target.id;
-	supplierUse[id] = !supplierUse[id];
-	localStorageStore("mansehrSupplierUse", JSON.stringify(supplierUse));
+$scope.updateLocalStore = function() {
+	localStorageStore("mansehrServiceUrls", JSON.stringify($scope.supplierUrls));
 }
 
 function searchSuppliers() {
-	for ( var i in supplierUrls) {
-		callSupplier(supplierUrls[i]);
-	}
+	angular.forEach($scope.supplierUrls, function callSupplier(sup) {
+		if(sup.use == false) {
+			sup.hits = {good : '*', tot : '*'};
+			return;
+		}
+		try {
+			sup.hits = {good : 'R', tot : 'R'};
+			
+			var req = new XMLHttpRequest();
+			req.open("GET", sup.url + $scope.movieName, true);
+			req.onload = function(e) {
+				callParserService(e, sup);
+			};
+			req.onerror = function(e) {
+				supplierError(e, sup);
+			};
+			req.send(null);
+		} catch (e) {
+			supplierError(e, sup);
+		}
+	});
 }
 
-function callSupplier(sup) {
-	if(supplierUse[sup.id] == false) {
-		return;
-	}
-	try {
-		var req = new XMLHttpRequest();
-		req.open("GET", sup.url + movieName, true);
-		req.onload = function(e) {
-			supplierLoaded(e, sup.id);
-		};
-		req.onerror = function(e) {
-			supplierError(e, sup.id);
-		};
-		req.send(null);
-	} catch (e) {
-		supplierError(e, sup.id);
-	}
+function supplierError(error, supplier) {
+	supplier.hits = {good : 'X', tot : 'X'};
+	debug(supplier.id + ', Result error: '+error);
 }
 
-function supplierLoaded(e, id) {
-	var res = new Object();
-	res.id = id;
-	res.xml = e.target.responseText;
-	callParserService(res);
-}
-
-function supplierError(e, id) {
-	var res = new Object();
-	res.id = id;
-	res.xml = 0;
-	resultArray.push(res);
-	debug(id + ', Result error: ' + resultArray.length);
-}
-
-function callParserService(sup) {
+function callParserService(e, sup) {
+	sup.hits.good = 'P';
+	sup.hits.tot = 'P';
+	
 	$.post(SERVICE_URI, {
 		"supplier" : sup.id,
-		"data" : sup.xml,
-		"movieName" : movieName
+		"data" : e.target.responseText,
+		"movieName" : $scope.movieName
 	// - Gör ingen filtrering på servern
 	}, 'json').done(function(data) {
-		parseJsonResult(data);
+		parseJsonResult(data, sup);
 	}).fail(function(jqxhr, textStatus, error) {
 		var err = textStatus + ', ' + error;
-		console.log("Request Failed: " + err);
+		supplierError(err, supplier);
 	}).always(function() {
 		debug('Callback: ' + sup.id);
+		
 	});
 }
 
-function parseJsonResult(xml) {
+function parseJsonResult(xml, supplier) {
 	var json = JSON.parse(xml);
 
-	$.each(json.goodMatchResult, function(key, val) {
-		data.goodMatchResult.push(val);
-	});
+	supplier.hits = {good : 0, tot : 0};
+	
+	if(json.goodMatchResult) {
+		$.each(json.goodMatchResult, function(key, val) {
+			val.logoUrl = supplier.logoUrl;
+			$scope.result.good.push(val);
+			supplier.hits.good++;
+			supplier.hits.tot++;
+		});
+	}
 
-	$.each(json.otherResult, function(key, val) {
-		data.otherResult.push(val);
-	});
+	if(json.otherResult) {
+		$.each(json.otherResult, function(key, val) {
+			val.logoUrl = supplier.logoUrl;
+			$scope.result.other.push(val);
+			supplier.hits.tot++;
+		});
+	}
+	
+	localStorageStore("movieSearchResult", JSON.stringify($scope.result));
+	$scope.updateLocalStore();
 
-	parseResult(data);
+	$scope.$apply();
 }
 
 function localStorageLoad(id) {
@@ -305,18 +232,41 @@ function localStorageClear() {
 	localStorage.clear();
 }
 
-function infoMsg(text) {
-	$("#infoMsg").html(text);
+var messageTimer;
+function showInfo(mess) {
+	$scope.message = mess;
+	$scope.errorMessage = false;
 }
 
-function showForm() {
-	$('#target').show();
-	$('#movie_name').focus();
+function showError(mess) {
+	$scope.message = mess;
+	$scope.errorMessage = true;
 }
 
-var dbgTxt = '';
+function resetMessage() {
+	$scope.message = '';
+	$scope.errorMessage = false;
+}
+
+$scope.debugMessage = 'DEV';
 function debug(txt) {
-	dbgTxt = dbgTxt + JSON.stringify(txt) + '<br/>';
-	if (DEBUG)
-		$('#debugField').html(dbgTxt);
+	$scope.debugMessage += JSON.stringify(txt) + '\n';
 }
+
+}).directive('focusMe', function() {
+  return {
+	   link: function(scope, element) {
+	        element[0].focus(); 
+	   }
+  	};
+}).directive('msResultList', function() {
+	return {
+	scope: {items: '='}, 
+	templateUrl: '/ms-result-list-item.html',
+		
+	link: function($scope, $element, $attrs) {
+		$scope.title = $attrs['title'];
+		$scope.orderby = $attrs['orderby'];
+	}
+	};
+});
